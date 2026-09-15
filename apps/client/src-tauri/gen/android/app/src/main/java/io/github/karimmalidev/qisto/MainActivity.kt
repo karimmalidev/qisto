51054ddd8037c6773a24183bcba5a684d626e736
Karim M. Ali <karimmalidev@gmail.com>
2026-09-15 06:07:02 +0300

    Setup Tauri

---
+++ apps/client/src-tauri/gen/android/app/src/main/java/io/github/karimmalidev/qisto/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.karimmalidev.qisto
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
